f7da0f84126148a35dae121bb62e83fe27cc1b9f
palomalindinha01 <palomalindinha35@gmail.com>
2023-10-16 16:06:47 -0300

    first commit

---
+++ questao09.py
@@ -0,0 +1,7 @@
+#faça um software que verifique entre 2 numeros qual o maior
+num01 = int(input("digite um número:"))
+num02 = int(input("digte o segundo número:"))
+num03 = num01-num02
+print (f"o número maior é:{num03}")              
+if num01>num02:
+    print(f"{num01}>{num02}")
